6fdfe0bacd3423a431b2fd1a64f764800345d18e
pandanlab <pandanlab@gmail.com>
2024-07-19 23:38:43 +0700

    step3: class component

---
+++ src/views/example/Mycomponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Mycomponent extends React.Component {
+    render(){
+        let a = 5;
+
+        return(
+            <h1>{a}</h1>
+        )
+    }
+}
+
+export default Mycomponent;
